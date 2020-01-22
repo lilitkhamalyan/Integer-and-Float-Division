ea03ab7f9c1931b3b036b73194599f322e4719b9
Lilit Khamalyan <lilitkhamalyan@gmail.com>
2020-01-22 13:39:57 -0500

    Integer and float division.

---
+++ division.py
@@ -0,0 +1,8 @@
+n1 = int(input("Enter the first number: "))
+n2 = int(input("Enter the second number: "))
+
+integerDivision = n1//n2
+print("Integer division:", integerDivision)
+
+floatDivision = n1/n2
+print("Float division:", floatDivision)
