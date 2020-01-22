8a568ac7ea8ba398e85f734c6f29cf9314ddcd60
Lilit Khamalyan <lilitkhamalyan@gmail.com>
2020-01-22 13:47:12 -0500

    Added comments.

--- division.py
+++ division.py
@@ -1,8 +1,11 @@
+# Get the numbers from the user
 n1 = int(input("Enter the first number: "))
 n2 = int(input("Enter the second number: "))
 
+# Calculates integer division and displays
 integerDivision = n1//n2
 print("Integer division:", integerDivision)
 
+# Calculates float division and displays
 floatDivision = n1/n2
 print("Float division:", floatDivision)
